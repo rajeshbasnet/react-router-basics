f7b9f2e448e89fa5e79ae290553730f513bb7c98
rajeshbasnet <rajeshkhapataribasnet123@gmail.com>
2023-10-31 19:16:18 +0545

    Small comment

--- frontend/src/pages/EventRoot.js
+++ frontend/src/pages/EventRoot.js
@@ -2,6 +2,7 @@ import React from "react";
 import EventsNavigation from "../components/EventsNavigation";
 import { Outlet } from "react-router-dom";
 
+/** Use Root Layout to create a outlet */
 function EventRootLayout() {
   return (
     <>
